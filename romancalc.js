230b7d7f026a16359cc59e09a0a42ec26ef20dba
Tanjin Prity <37024552+tanjinprity4@users.noreply.github.com>
2020-12-28 01:57:45 -0500

    Phase 5: Complete a function to convert integers to Roman Numerals
    Putting it all together: Now that we have our functions all set, all we have to do is put them together.
    
    1. Move functions from phases 3 and 4 above the get_args function of the file (so that the functions can be called later).
    2. Edit get_args function so that it converts any Roman Numeral inputs into integers before storing (we can use the isNumber function here).
    3. Always display both the integer and Roman Numeral version of the results for accessibility.

--- romancalc.js
+++ romancalc.js
@@ -1,64 +1,10 @@
 /*
-Create a function called get_args that, 
-quite literally, gets arguments from the user 
-and returns the arguments. 
-The user will provide these arguments in the command line 
-when they run their code.
-*/
-function get_args(){
-    let args = [];  // Setting a variable to empty array
-    var j;   // Initialize a variable
-
-    /* 
-    Initialize a for loop that iterates from 2 
-    to length of the arguments passed in command line.
-    Initialized to 2 because the arguments provided by the users 
-    start from index 2 in process.argv.
-    */
-    for (j = 2; j < process.argv.length; j++) {
-        args.push(process.argv[j]); // Adding the arguments to array
-    }  // End of loop
-    return args;
-}  // End of function
-
-/*
- Calculates using operands and operator from the user 
- and returns the result.
-*/
-function calculate(){
-    // Initialize variables with the second, third, and fourth arguments from user input.
-    let op = get_args();
-    const firstOperand = Number(op[0]);
-    const secondOperand = Number(op[2]);
-    const operator = op[1];
-
-    // Initialize variable to hold result of operations.
-    var result;
-
-    // Conditions to determine what the operation is from the user input and calculate the result.
-    if (operator === '+') { // Addition
-        result = firstOperand + secondOperand;
-    } else if (operator === '-') {  // Subtraction
-        result = firstOperand - secondOperand;
-    } else if (operator === 'x') {  // Multiplication
-        result = firstOperand * secondOperand;
-    } else if (operator === '/') {  // Division
-        result = firstOperand / secondOperand;
-    } else if (operator === '^') {  // Exponents
-        result = firstOperand ** secondOperand;
-    } else if (operator === '%') {  // Modulus
-        result = firstOperand % secondOperand;
-    } else if (operator === '<<') {  // Shift left
-        result = firstOperand << secondOperand;
-    } else if (operator === '>>') {  // Shift right
-        result = firstOperand >> secondOperand;
-    } else { // Error management
-        console.log("Operator not recognized. Please enter a valid operator.");
-        console.log("Valid Operators are: \nAddition: +, Subtraction: -, Multiplication: x, Division: /, Exponents: ^, Modulus: %, Shift left: <<, Shift right: >>");
-    }
-
-    return result;
-}  // End of function
+This function returns true (0) for input that is a number and 
+false for anything else, and 
+will help us identify if user input is an integer or a Roman Numeral.*/
+function isNumber(n) { 
+    return /^-?[\d.]+(?:e-?\d+)?$/.test(n); 
+}
 
 /*
 Takes a string of characters(Roman letters) as input and 
@@ -146,10 +92,82 @@ function intToRoman(int) {
 
     return roman;
 }     // End of function
+ 
+
+/*
+Create a function called get_args that, 
+quite literally, gets arguments from the user 
+and returns the arguments. 
+The user will provide these arguments in the command line 
+when they run their code.
+*/
+function get_args(){
+    let args = [];  // Setting a variable to empty array
+    var j;   // Initialize a variable
+
+    /* 
+    Initialize a for loop that iterates from 2 
+    to length of the arguments passed in command line.
+    Initialized to 2 because the arguments provided by the users 
+    start from index 2 in process.argv.
+    */
+    for (j = 2; j < process.argv.length; j++) {
+        // Storing the argument in the current iteration to use repeatedly.
+        var argument = process.argv[j];
+        /* Checking if the user operand input is a number. 
+        If not, convert operand(s) to number [from roman numeral].
+        Does not change operator. */
+        if (j != 3 && !isNumber(argument)){
+            argument = romanToInt(argument);
+        }
+        args.push(argument); // Adding the arguments to array
+    }  // End of loop
+    return args;
+}  // End of function
+
+/*
+ Calculates using operands and operator from the user 
+ and returns the result.
+*/
+function calculate(){
+    // Initialize variables with the second, third, and fourth arguments from user input.
+    let op = get_args();
+    const firstOperand = Number(op[0]);
+    const secondOperand = Number(op[2]);
+    const operator = op[1];
+
+    // Initialize variable to hold result of operations.
+    var result;
+
+    // Conditions to determine what the operation is from the user input and calculate the result.
+    if (operator === '+') { // Addition
+        result = firstOperand + secondOperand;
+    } else if (operator === '-') {  // Subtraction
+        result = firstOperand - secondOperand;
+    } else if (operator === 'x') {  // Multiplication
+        result = firstOperand * secondOperand;
+    } else if (operator === '/') {  // Division
+        result = firstOperand / secondOperand;
+    } else if (operator === '^') {  // Exponents
+        result = firstOperand ** secondOperand;
+    } else if (operator === '%') {  // Modulus
+        result = firstOperand % secondOperand;
+    } else if (operator === '<<') {  // Shift left
+        result = firstOperand << secondOperand;
+    } else if (operator === '>>') {  // Shift right
+        result = firstOperand >> secondOperand;
+    } else { // Error management
+        console.log("Operator not recognized. Please enter a valid operator.");
+        console.log("Valid Operators are: \nAddition: +, Subtraction: -, Multiplication: x, Division: /, Exponents: ^, Modulus: %, Shift left: <<, Shift right: >>");
+    }
+
+    return result;
+}  // End of function
+
 
 
 // Testing phase 1
-let op = get_args();  // Code to check that the get_args function returns an array
+// let op = get_args();  // Code to check that the get_args function returns an array
 // Prints arguments to the terminal.
 // console.log("Phase 1 test");
 // console.log("The first operand is: ", op[0]);
@@ -164,4 +182,16 @@ let op = get_args();  // Code to check that the get_args function returns an arr
 // console.log("Roman Numerals to Int conversion:", romanToInt(process.argv[2]));
 
 // Testing Phase 4
-console.log("Int to Roman Numerals conversion:", intToRoman(process.argv[2]));
+// console.log("Int to Roman Numerals conversion:", intToRoman(process.argv[2]));
+
+// Testing Phase 5
+let op = get_args();
+var result = calculate();
+// Check if the result is integer. 
+// If it is, get the roman numeral representing integer result.
+if (Number.isInteger(result)) {
+    result += ", " + intToRoman(result);
+}
+
+// Give output to user.
+console.log("The result is:", result);
